071e0cdb0463504de208ba65dae6234132b93076
vijoy2019 <46294383+vijoy2019@users.noreply.github.com>
2019-01-24 11:22:58 +0530

    Testing scala added

---
+++ SetSpec.scala
@@ -0,0 +1,24 @@
+import org.scalatest.FlatSpec
+
+class SetSpec extends FlatSpec {
+
+  "An empty Set" should "have size 0" in {
+    assert(Set.empty.size == 0)
+  }
+
+  it should "produce NoSuchElementException when head is invoked" in {
+    assertThrows[NoSuchElementException] {
+      Set.empty.head
+      //Set.empty.tail
+    }
+  }
+
+
+    it should "produce NoSuchElementException when tail is invoked" in {
+      assertThrows[NoSuchElementException] {
+        //Set.empty.head
+        Set.empty.tail
+        if (Set != null) { "hellp partha,how  r u "}
+      }
+    }
+}
---
+++ SetSpec1.scala
@@ -0,0 +1,27 @@
+import org.scalatest._
+import prop._
+import scala.collection.immutable._
+
+
+class SetSpec1 extends PropSpec with TableDrivenPropertyChecks with Matchers {
+
+  val examples =
+    Table(
+      "set",
+      BitSet.empty,
+      HashSet.empty[Int],
+      TreeSet.empty[Int]
+    )
+
+  property("an empty Set should have size 0") {
+    forAll(examples) { set =>
+      set.size should be (0)
+    }
+  }
+
+  property("invoking head on an empty set should produce NoSuchElementException") {
+    forAll(examples) { set =>
+      a [NoSuchElementException] should be thrownBy { set.head }
+    }
+  }
+}
